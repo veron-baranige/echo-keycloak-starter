5d0fadd5bf6a092b35832f014eef935777e485b0
Veron Baranige <veron.b@hsenidoutsourcing.com>
2024-03-24 15:28:42 +0530

    Add logging with slog & tint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"log"
+	"log/slog"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/lmittmann/tint"
 	echoSwagger "github.com/swaggo/echo-swagger"
 	_ "github.com/veron-baranige/echo-keycloak-starter/docs/swagger"
 	"github.com/veron-baranige/echo-keycloak-starter/internal/auth"
@@ -23,21 +26,28 @@ import (
 // @host http://localhost:8080
 // @BasePath /api
 func main() {
+	setupLogger()
+
 	if err := config.LoadEnv("."); err != nil {
 		log.Fatal(err)
 	}
+	slog.Info("Loaded configurations")
 
 	if err := db.SetupClient(); err != nil {
 		log.Fatal(err)
 	}
+	slog.Info("Established database connection")
 	
 	auth.SetupKeycloakClient()
+	slog.Info("Finished setting up keycloak client")
 	
 	e := echo.New()
 	setupMiddleware(e)
 	setupRoutes(e)
 	
-	e.Logger.Fatal(e.Start(":" + config.Get(config.ServerPort)))
+	if err := e.Start(":" + config.Get(config.ServerPort)); err != nil {
+		slog.Error("Failed to start the server", "err", err)
+	}
 }
 
 func setupMiddleware(e *echo.Echo) {
@@ -48,4 +58,10 @@ func setupMiddleware(e *echo.Echo) {
 func setupRoutes(e *echo.Echo) {
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	routes.SetupAuthRoutes(e)
+}
+
+func setupLogger() {
+	w := os.Stderr
+	logger := slog.New(tint.NewHandler(w, nil))
+	slog.SetDefault(logger)
 }
